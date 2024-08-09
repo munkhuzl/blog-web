23e3c55b38c86bf89bb2bb181a8abb6e9ed45b77
24LP1786 24LP1786 <24LP1786@Users-iMac-3.local>
2024-08-09 14:46:50 +0800

    404page

--- app/[author]/[slug]/page.js
+++ app/[author]/[slug]/page.js
@@ -1,33 +1,35 @@
-import {Footer} from '@/components/footer';
-import { Navbar } from '@/components/navbar';
-import parse from 'html-react-parser';
+import { Footer } from "@/components/footer";
+import { Navbar } from "@/components/navbar";
+import parse from "html-react-parser";
 
+// async function generateMetadata({ params }) {
+//   const response = await fetch(
+//     `https://dev.to/api/articles/${params.author}/${params.slug}`
+//   );
+//   const article = await response.json();
+//   return {
+//     openGraph: {
+//       title: article.title,
+//       description: article.description,
+//       images: [{ url: article.social_image }],
+//     },
+//   };
+// }
 
+export default async function Page({ params }) {
+  const response = await fetch(
+    `https://dev.to/api/articles/${params.author}/${params.slug}`
+  );
+  const article = await response.json();
 
-
-export default async function generateMetadata ({params}){
-    const response = await fetch(`https://dev.to/api/articles/${params.author}/${params.slug}`)
-    const article= await response.json();
-return {
-  openGraph: { 
-    title: article.title,
-    description: article.description,
-    images: [{url: article.social_image}],
-  }
-};
-}
-export default async function Page({params}) {
-  const response = await fetch(`https://dev.to/api/articles/${params.author}/${params.slug}`)
-  const article= await response.json();
-
-    return (
+  return (
     <main>
-      <Navbar/>
-      <div className="container mx-auto"> 
+      <Navbar />
+      <div className="container mx-auto">
         <div>{article.title}</div>
         <div className="prose">{parse(article.body_html)}</div>
       </div>
-      <Footer/>
-      </main>
-);
+      <Footer />
+    </main>
+  );
 }
--- app/page.js
+++ app/page.js
@@ -1,38 +1,20 @@
 "use client";
 import { Navbar } from "@/components/navbar";
 import React from "react";
-<<<<<<< HEAD:app/page.js
-=======
 import { AllBlog } from "@/components/allblog";
 import Footer from "@/components/footer";
-
-import Link from "next/link";
-import Trending from "./[author]";
->>>>>>> f06f67f92bbeb5ae53b99fae17a461a054eed541:pages/index.js
-
-import Footer from "@/components/footer";
+import { Slidernews } from "@/pages/api/component/slider";
 import Trending from "./blog/page";
 
-// import UncontrolledExample, { Swipe } from "./api/component/slider";
-import { Slidernews } from "@/pages/api/component/slider";
-import { AllBlog } from "@/components/allblog";
 export default function Home() {
   return (
     <main>
       <div className="bg-white">
-        {/* <Link href={"/about"}>About</Link>
-        <Link href={"/blog"}>Blog</Link>
         <Link href={"/contact"}>Contact</Link> */}
         <Navbar />
-<<<<<<< HEAD:app/page.js
-        <Slidernews/>
+        <Slidernews />
         {/* <Page2 />  */}
         <Trending />
-        <AllBlog/>
-=======
-
-        <Trending />
-        {/* <Body/> */}
         <AllBlog />
 >>>>>>> f06f67f92bbeb5ae53b99fae17a461a054eed541:pages/index.js
         <Footer />
